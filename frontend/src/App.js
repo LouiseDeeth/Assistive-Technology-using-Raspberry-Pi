44bbe507d32d3c359db1843fe7c5b4732f8b7e59
Louise Deeth <louisedeeth@gmail.com>
2025-03-05 22:46:22 +0000

    Set up Flask backend and React frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
+
+    return (
+        <div>
+            <h1>Assistive Technology</h1>
+            <p>Backend: {message}</p>
+        </div>
+    );
 }
 
 export default App;
