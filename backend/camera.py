7df5c061c07bf82c7603544326c820074534fa1d
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-03-18 20:19:15 +0000

    camera.py todo list

---
+++ backend/camera.py
@@ -0,0 +1,16 @@
+# import opencv, base64, numpy
+
+# use .VideoCapture for default camera
+
+# loop while true and read feed from camera
+
+# image show camera feed
+
+# press key to exit
+
+# release and destroy windows
+
+# resize image 
+
+# convert to base64 for api usage
+ 
