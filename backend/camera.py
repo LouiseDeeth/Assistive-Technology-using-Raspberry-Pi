364c3e8272794a2fdbf4b8f2d4d913c3fbc710a5
Sarah O Connor <sarahaoconnor10@gmail.com>
2025-03-21 19:19:15 +0000

    implementing initial functionality to capture camera frames

--- backend/camera.py
+++ backend/camera.py
@@ -1,8 +1,20 @@
 # import opencv, base64, numpy
+import cv2
+import base64
 
-# use .VideoCapture for default camera
+def get_frames():
+    while True: # loop while true and read feed from camera
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            _, buffer = cv2.imencode(".jpg", frame)
+            frame_bytes = buffer.tobytes()
+            yield(b"--frame\r\n"
+                  b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
 
-# loop while true and read feed from camera
+
+
 
 # image show camera feed
 
