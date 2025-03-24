67dbfda8dd098200d73bb669d839f7d7625db237
Louise Deeth <louisedeeth@gmail.com>
2025-03-24 11:58:10 +0000

    Implementing dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
 import CameraPage from "./pages/CameraPage";
-import FavouritesPage from "./pages/FavoritesPage";
+import FavoritesPage from "./pages/FavoritesPage";
 import AlphabetPage from "./pages/AlphabetPage";
 import Navbar from "./components/Navbar";
 import "./App.css"; 
@@ -11,9 +11,18 @@ import { ThemeProvider } from "@mui/material/styles";
 import styled from "@emotion/styled";
 
 function App() {
+    const [darkMode, setDarkMode] = useState(() => {
+        const saved = localStorage.getItem("darkMode");
+        return saved === "true";
+    });
+
     const [message, setMessage] = useState("");
 
     useEffect(() => {
+        localStorage.setItem("darkMode", darkMode);
+    }, [darkMode]);
+
+    useEffect(() => {     
         fetch("http://127.0.0.1:5000/")
             .then((res) => res.json())
             .then((data) => setMessage(data.message))
@@ -21,15 +30,17 @@ function App() {
     }, []);
 
     return (
+        <div className={darkMode ? "dark" : ""}>
         <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<ProfilePage />} />
-                <Route path="/camera" element={<CameraPage />} />
-                <Route path="/favourites" element={<FavouritesPage />} />
-                <Route path="/alphabet" element={<AlphabetPage />} />
-            </Routes>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProfilePage darkMode={darkMode} setDarkMode={setDarkMode} />} />
+            <Route path="/camera" element={<CameraPage darkMode={darkMode} />} />
+            <Route path="/favorites" element={<FavoritesPage darkMode={darkMode} />} />
+            <Route path="/alphabet" element={<AlphabetPage darkMode={darkMode} />} />
+          </Routes>
         </Router>
+      </div>
     );
 }
 
