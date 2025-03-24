2723870a36beda538633948dfac9dd6fd09eea62
Louise Deeth <louisedeeth@gmail.com>
2025-03-24 11:58:10 +0000

    Implementing dark mode

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { Switch } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-function ProfilePage() {
-    const [darkMode, setDarkMode] = useState(false);
+function ProfilePage({ darkMode, setDarkMode }) {
     const navigate = useNavigate();
 
     return (
@@ -15,19 +14,18 @@ function ProfilePage() {
                 <p> I communicate using sign language. Excited to connect and learn together!</p>
                 <p>Feel free to interact with me using sign or text!</p>
             </div>
+            
             <div>
                 <label className="LightDarkToggle">
                     Dark/Light Mode
                     <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
                 </label>
             </div>
-            <div>
+
+            <div className="profile-buttons">
                 <button className="logout-btn">Logout</button>
-            </div>
-            <div>
                 <button className="delete-btn">Delete Account</button>
-            </div>
-           
+            </div>         
         </div>
     );
 }
