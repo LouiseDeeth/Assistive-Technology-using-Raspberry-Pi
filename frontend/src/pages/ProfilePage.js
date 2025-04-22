0020e0e185d5863e8d2efd85dfb64effec9df9e1
Louise Deeth <louisedeeth@gmail.com>
2025-04-22 23:44:30 +0100

    Clean up code

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
 import { Switch } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 
 function ProfilePage({ darkMode, setDarkMode }) {
-    const navigate = useNavigate();
 
     return (
         <div className={`profile-page ${darkMode ? "dark" : ""}`}>
             <div className="profile-image-container">
                 <h2 class="app-title">Assistive Technology App</h2>
-                <img src="/images/picture1.png" alt="Sign Language Picture" className="Picture1" />
+                <img src="/images/picture1.png" alt="Sign Language" className="Picture1" />
 
                 <h4>Hi there! </h4>
                 <h4> I communicate using sign language. Excited to connect and learn together!</h4>
